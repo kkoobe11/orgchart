b730781b5d8f21a5cfb89661fe80d538451f9bc8
Kobe Bantes <102146326+kkoobe11@users.noreply.github.com>
2024-02-07 09:37:39 +0800

    push

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import HomePage from "@/containers/home/page"
 
 export default function Home() {
   return (
-      <div className="hidden max-w-screen-xl p-10 px-4 pb-16 mx-auto space-y-6 md:block">
+      <div className="hidden max-w-screen-xl px-4 pb-16 mx-auto space-y-6 md:block">
         <div className="flex flex-col space-y-8 lg:space-x-12 lg:space-y-0">
         <HomePage></HomePage>
 </div>
